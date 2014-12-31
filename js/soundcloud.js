fbe35009fe1691882a4c6c342fc4f5afd4e72c95
Chang Liu <shampliu@gmail.com>
2014-12-31 02:14:40 -0800

    Updated CSS and FA icons

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -18,13 +18,31 @@ $(document).ready(function() {
 $('button').click(function(){
 	event.preventDefault();
 	$genre = $('#search-term').val();
-	SC.get('/tracks', { genres: $genre }, function(tracks) {
-	console.log(tracks);
+	SC.get('/tracks', { genres: $genre}, function(tracks) {
+	 console.log(tracks);
 
-	$('#results').html('');
-    $(tracks).each(function(index, track) {
-      $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
-    });
+  	$('#results').html(''); // clears the list before refreshing
+      $(tracks).each(function(index, track) {
+        if (track.playback_count > 1000000) {
+          // SC.oEmbed("https://soundcloud.com/markverse/miami-feat-hakan-preview-mark-verse", { auto_play: false }, function(oEmbed) {
+          //   console.log('oEmbed response: ' + oEmbed);
+          // });
+          $('#results').append($('<li></li>').html(track.title + ' - ' + track.playback_count + ' - ' + track.permalink_url));
+  //         $('#results').append($('<li></li>').html(<script>SC.oEmbed(track.permalink_url { auto_play: false }, function(oEmbed) {
+  // console.log('oEmbed response: ' + oEmbed</script>);
+  //         });
+      
 
-})
+          
+
+
+        }
+
+
+        
+
+
+      });
+
+  })
 });
